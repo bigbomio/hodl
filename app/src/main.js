c9b22274e816d9be7560c925d15d039d1829de60
Tran Tho <trantho3910@gmail.com>
2018-11-01 15:29:07 +0700

    app component join

--- app/src/main.js
+++ app/src/main.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import ProgramContainer from './layouts/program/programContainer'
-import Dashboard from     './layouts/dashboard/Dashboard'
-import Me from            './layouts/me/me'
+import Join from            './layouts/join/join'
 import Introduce from     './layouts/introduce/introduce'
 
 
 export default () => (
     <Switch>
     <Route exact path="/" component={Introduce} />  
-    <Route exact path="/program" component={ProgramContainer} />
-    <Route exact path="/git" component={Dashboard} />
-    <Route exact path="/me" component={Me} />
+    <Route exact path="/join" component={Join} />
+    <Route exact path="/metamask" component={ProgramContainer} />
   </Switch>
 )
